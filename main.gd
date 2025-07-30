de2e28b97b926aa10484fb4793a02c13234295e0
il331 <126287200+il331@users.noreply.github.com>
2025-07-30 15:51:08 -0300

    Updated some files
    Changed the main menu so you operate by clicking the menu

--- main.gd
+++ main.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-func _physics_process(delta: float) -> void:
-	
-	if Input.is_action_pressed("one"):
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_pressed("left_click"):
 		get_tree().change_scene_to_file("res://level_1.tscn")
-	
-	if Input.is_action_pressed("two"):
+
+
+func _on_exitbox_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_pressed("left_click"):
 		get_tree().quit()
